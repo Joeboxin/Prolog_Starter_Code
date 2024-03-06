91f3e2e55b4bc87ddde838beacece2f9eab2e5b1
Joeboxin <joey.hong26036@gmail.com>
2024-03-06 15:41:17 -0500

    Blamo

--- descendant.pl
+++ descendant.pl
@@ -1,23 +1,15 @@
 % Declare your facts here
-parent(John, Jim).
-parent(John, Lisa).
-parent(John, James).
-parent(Bob, Mike).
-parent(Bob, Ann).
-parent(Tom, Sarah).
-parent(David, Ike).
-parent(Mark, Sam).
-parent(Mark, Emma).
-parent(James, David).
-parent(James, Olivia).
-
-Your goal is to define a rule called ’descendant(X,Y)’ that queries your knowledge base to verify if
-X is a descendant of Y. Incorrectly spelling the rule name will result in a loss of points because the
-autograder will not recognize the typo. You are expected to construct the knowledge base within the
-same descendant.pl file. Do NOT construct it in a separate .txt or .csv file and import it. This will
-cause issues when grading your submission.
-CMPSC 360, Spring 2024, Extra Credit 2
-
+parent('John', 'Jim').
+parent('John', 'Lisa').
+parent('John', 'James').
+parent('Bob', 'Mike').
+parent('Bob', 'Ann').
+parent('Tom', 'Sarah').
+parent('David', 'Ike').
+parent('Mark', 'Sam').
+parent('Mark', 'Emma').
+parent('James', 'David').
+parent('James', 'Olivia').
 
 
 % Complete the following horn clause
